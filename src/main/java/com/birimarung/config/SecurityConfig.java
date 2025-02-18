047b9ce169d2f3bc8edd4ccdc76feac318c1ab73
Ndivho <ndibo69@gmail.com>
2025-02-18 19:59:37 +0200

    Added Security Filter For certain endpoints.

--- src/main/java/com/birimarung/config/SecurityConfig.java
+++ src/main/java/com/birimarung/config/SecurityConfig.java
@@ -13,8 +13,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .requestMatchers("/products/**").permitAll() // Allow access to /public/** endpoints without authentication
-                .requestMatchers("/admin/**").authenticated() // Require authentication for /admin/** endpoints
+                .requestMatchers("/products/**").permitAll()
+                .requestMatchers("/admin/**").authenticated()
                 .anyRequest().authenticated();
 
         return http.build();
