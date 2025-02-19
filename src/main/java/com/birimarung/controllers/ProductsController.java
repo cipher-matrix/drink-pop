a29d59a4ea52bba088326c79b57183b848d737ce
Ndivho <ndibo69@gmail.com>
2025-02-19 13:14:23 +0200

    Addes redis cache for Products , request time went from 129ms to 6ms

--- src/main/java/com/birimarung/controllers/ProductsController.java
+++ src/main/java/com/birimarung/controllers/ProductsController.java
@@ -4,12 +4,16 @@ import com.birimarung.dto.RestResponse;
 import com.birimarung.redis_data.ProductsRedis;
 import com.birimarung.services.ProductsService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/products")
@@ -18,9 +22,15 @@ public class ProductsController {
     private final ProductsService productsService;
 
     @GetMapping("/all")
-    public RestResponse<List<ProductsRedis>> getAllProductsController() {
-        return productsService.getAllProducts();
+    public ResponseEntity<RestResponse<List<ProductsRedis>>> getAllProductsController() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setCacheControl(CacheControl.maxAge(1, TimeUnit.HOURS).cachePublic());
+        RestResponse<List<ProductsRedis>> response = productsService.getAllProducts();
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(response);
     }
+
 
     @GetMapping("/all/{drink_name}")
     public RestResponse<List<ProductsRedis>> getDrinksOfCertainName(@PathVariable String drink_name) {
