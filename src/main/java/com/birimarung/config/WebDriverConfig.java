04dd6accc65a6368656acb7be8c74ae3925aff8b
Ndivho <ndibo69@gmail.com>
2025-02-24 23:48:33 +0200

    Downloaded chrome on droplet and addles headless arguments for chrome cronjob executing.

--- src/main/java/com/birimarung/config/WebDriverConfig.java
+++ src/main/java/com/birimarung/config/WebDriverConfig.java
@@ -3,8 +3,10 @@ package com.birimarung.config;
 import jakarta.annotation.PreDestroy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.File;
 import java.time.Duration;
 
 
@@ -14,18 +16,25 @@ public class WebDriverConfig {
     public WebDriver getDriver() {
         ChromeOptions options = new ChromeOptions();
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        options.setBinary("/usr/bin/google-chrome");
+        options.addArguments("--remote-debugging-port=9515");
         options.addArguments("--incognito");
         options.addArguments("--headless");
         options.addArguments("--disable-gpu");
-        options.addArguments("--remote-debugging-port=9222");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-blink-features=AutomationControlled");
         options.addArguments("--disable-extensions");
         options.addArguments("--blink-settings=imagesEnabled=false");
         options.addArguments("--disable-notifications");
-        driver = new ChromeDriver(options);
+        ChromeDriverService service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(new File("/usr/local/bin/chromedriver"))
+                .usingAnyFreePort()
+                .build();
+
+        WebDriver driver = new ChromeDriver(service, options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
         return driver;
     }
 
